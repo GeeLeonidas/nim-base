5c415ee16626780ffd564d89058f37909bd97223
Guilherme Leoi <geeleonidas@pm.me>
2023-10-06 00:05:14 -0300

    Add library support

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
+let
+  # Add libraries
+  libs = [
+    # ...
+  ];
+in
 mkShell {
   # Add system dependencies
   packages = [
@@ -10,10 +16,12 @@ mkShell {
     nim2
     nimble-unwrapped
   ];
+  buildInputs = libs;
 
   # Bash statements that are executed by nix-shell
   shellHook = ''
     export CC="clang"
     export CXX="clang++"
+    export LD_LIBRARY_PATH="${lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
   '';
 }
