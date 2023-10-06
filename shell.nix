012063fa21d9184c41ff58ce3979688fa7778858
Guilherme Leoi <geeleonidas@pm.me>
2023-10-06 18:55:11 -0300

    Add Nix flake support

--- shell.nix
+++ shell.nix
@@ -1,28 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-let
-  # Add libraries
-  libs = [
-    # ...
-  ];
-in
-mkShell {
-  # Add system dependencies
-  packages = [
-    gitFull
-    clang
-    mold
-    gdb
-    nim2
-    nim2Packages.nimble
-  ];
-  buildInputs = libs;
-
-  # Bash statements that are executed by nix-shell
-  shellHook = ''
-    export CC="clang"
-    export CXX="clang++"
-    export LD_LIBRARY_PATH="${lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
-  '';
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
